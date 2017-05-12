73b671e99f342416e06efc7927e603db5e47b71d
Chen, Zhuhui <chenzhuhui@idaoben.com>
2017-05-12 21:34:46 +0800

    Output project video instead of image sequences.

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ out_clip = clip.fl_image(
     lambda img: cv2.cvtColor(pipeline.search_cars(cv2.cvtColor(img, cv2.COLOR_RGB2BGR),
                                                   region_of_interest=((0, 400), (1280, 656))),
                              cv2.COLOR_BGR2RGB))
-# out_clip.write_videofile(clip_output_path, audio=False)
-out_clip.write_images_sequence("debug2/frame_%04d.jpg")
+out_clip.write_videofile(clip_output_path, audio=False)
+# out_clip.write_images_sequence("debug2/frame_%04d.jpg")
